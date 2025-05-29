14a99a51cc2355e283b7f829681b254de3ba9543
Eldon Mathias <eldon@emathias.com>
2025-05-29 15:30:12 -0500

    fix notification bar appearance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "com.emathias.periodic"
-        minSdk = 26
+        minSdk = 27
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
